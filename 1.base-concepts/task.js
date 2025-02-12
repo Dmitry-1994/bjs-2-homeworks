25a27ff6b6b546f2abcc8b3bf02ef30d28502860
Dmitry Tarasov <vizalyyy@gmail.com>
2025-02-13 01:31:50 +0500

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant = Math.pow(b, 2) - 4 * a * c;
+
+	if (discriminant === 0) {
+		arr.push(-b / (2 * a));
+	} else if (discriminant > 0) {
+		arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+		arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+
+	}
+
+	return arr;
+
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let percentOfMonth = (percent / 100) / 12;
+	let bodyLoan = amount - contribution;
+	let paymentOfMonth = bodyLoan * (percentOfMonth + (percentOfMonth / ((Math.pow((1 + percentOfMonth), countMonths)) - 1)));
+	let amountTotal = (paymentOfMonth * countMonths).toFixed(2);
+
+	return parseFloat(amountTotal);
+
 }
