e0a24f30bffc079e7f83abad6893e60586ce4fbd
Dmitry Tarasov <vizalyyy@gmail.com>
2025-02-14 01:45:31 +0500

    Функции. Выполнено задание 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,21 @@
+"use strict"
+
 function getArrayParams(...arr) {
+  let min = Infinity
+  let max = - Infinity
+  const sumArr = arr.reduce((accumulator, currentValue) => accumulator + currentValue)
+
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] <= min) {
+      min = arr[i];
+    }
+
+    if (arr[i] >= max) {
+      max = arr[i];
+    }
+  }
+
+  let avg = parseFloat((sumArr / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
