8c35ccdce6f593700769295a985f773c369b1e3d
Dmitry Tarasov <vizalyyy@gmail.com>
2025-02-18 00:46:23 +0500

    Массивы. Выполнено первое задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,4 +1,14 @@
+"use strict"
+
 function compareArrays(arr1, arr2) {
+    const array1 = Array.from(arr1);
+    const array2 = Array.from(arr2);
+
+    if (array1.length === array2.length) {
+        return array1.every((element, index) => array2[index] === element);
+    } else {
+        return false;
+    } 
   
 }
 
