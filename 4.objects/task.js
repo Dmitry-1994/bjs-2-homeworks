561b5efaa8c4718aa939d27f1b28940b620eb386
Dmitry Tarasov <vizalyyy@gmail.com>
2025-02-23 15:05:21 +0500

    Объекты. SetSubject

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,9 +1,14 @@
+"use strict"
+
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
@@ -17,3 +22,11 @@ Student.prototype.getAverage = function () {
 Student.prototype.exclude = function (reason) {
   
 }
+
+
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
+student1.setSubject("Algebra");
+student2.setSubject("Geometry");
+console.log(student1);
+console.log(student2);
