9fb542d52a425b4e617fcffb8f656b5e52eb9b79
Dmitry Tarasov <vizalyyy@gmail.com>
2025-02-25 02:53:31 +0500

    Классы. Выполнено первое задание

---
+++ 5.classes/task.js
@@ -0,0 +1,68 @@
+"use strict"
+
+class PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.pagesCount = pagesCount;
+		this._state = 100;
+		this.type = null;
+	}
+
+	fix() {
+		this.state *= 1.5;
+	}
+
+	set state(state) {
+		if (state < 0) {
+			this._state = 0;
+		} else if (state > 100) {
+			this._state = 100;
+		} else {
+			this._state = state;
+		}
+	}
+
+	get state() {
+		return this._state;
+	}
+}
+
+class Magazine extends PrintEditionItem {
+	constructor(name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "magazine";
+	}
+}
+
+class Book extends PrintEditionItem {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "book";
+		this.author = author;
+	}
+}
+
+class NovelBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "novel";
+		this.author = author;
+	}
+}
+
+class FantasticBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "fantastic";
+		this.author = author;
+	}
+}
+
+class DetectiveBook extends Book {
+	constructor(author, name, releaseDate, pagesCount) {
+		super(name, releaseDate, pagesCount);
+		this.type = "detective";
+		this.author = author;
+	}
+}
