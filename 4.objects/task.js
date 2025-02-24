fd27b5b9149c2a0dd6c7b582ec0664bedc377736
Dmitry Tarasov <vizalyyy@gmail.com>
2025-02-24 23:49:53 +0500

    Объекты. Задание выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,32 +1,46 @@
 "use strict"
 
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (!this.marks) {
+		return;
+	}
+
+	if (marks.length !== 0) {
+		this.marks.push(...marks);
+	}
+
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks) {
+		return 0;
+	} else {
+		return this.marks.reduce((acc, item, index, arr) => {
+			acc += item;
+			if ((index === arr.length - 1) && (arr.length !== 0)) {
+				return acc / arr.length;
+			}
+			return acc;
+
+		}, 0);
+	}
+
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
+
 }
-
-
-let student1 = new Student("Василиса", "женский", 19);
-let student2 = new Student("Артём", "мужской", 25);
-student1.setSubject("Algebra");
-student2.setSubject("Geometry");
-console.log(student1);
-console.log(student2);
