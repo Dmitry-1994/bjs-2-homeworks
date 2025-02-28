00f790e75377a23e979e577e523727162cc6c26e
Dmitry Tarasov <vizalyyy@gmail.com>
2025-02-28 15:50:16 +0500

    Исключения. Выполнено первое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,18 @@
-﻿
+﻿"use strict"
+
+function parseCount(number) {
+	const parseNumber = Number.parseFloat(number);
+	if (Number.isNaN(parseNumber)) {
+		throw new Error("Невалидное значение")
+	}
+	return parseNumber;
+}
+
+function validateCount(number) {
+	try {
+		const parseNumber = parseCount(number);
+		return parseNumber;
+	} catch (error) {
+		return error;
+	}
+}
