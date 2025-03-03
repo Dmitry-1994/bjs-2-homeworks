1436243d5ff6d50e881e3760a74de32db09405b9
Dmitry Tarasov <vizalyyy@gmail.com>
2025-03-03 16:48:25 +0500

    Ассинхронность. Реализован метод getCurrentFormattedTime

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,26 +1,35 @@
+"use strict"
+
 class AlarmClock {
-    constructor() {
-        this.alarmCollection = [];
-        this.intervalId = null;
-    }
+	constructor() {
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
 
-    addClock(startAlarm, callback) {
-        if (startAlarm === null || callback === undefined) {
-            throw new Error("Отсутствуют обязательные аргументы")
-        }
+	addClock(startAlarm, callback) {
+		if (startAlarm === null || callback === undefined) {
+			throw new Error("Отсутствуют обязательные аргументы")
+		}
 
-        if(this.alarmCollection.find(alarm => alarm.time === startAlarm)) {
-            console.warn("Уже присутствует звонок на это же время");
-        }
+		if (this.alarmCollection.find(alarm => alarm.time === startAlarm)) {
+			console.warn("Уже присутствует звонок на это же время");
+		}
 
-        this.alarmCollection.push({
-            callback : callback,
-            time : startAlarm,
-            canCall : true
-        });
-    }
+		this.alarmCollection.push({
+			callback: callback,
+			time: startAlarm,
+			canCall: true
+		});
+	}
 
-    removeClock(time) {
-        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
-    }
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+	}
+
+	getCurrentFormattedTime() {
+		const date = new Date();
+		const hours = date.getHours().toString().padStart(2, "0");
+		const minutes = date.getMinutes().toString().padStart(2, "0");
+		return `${hours}:${minutes}`;
+	}
 }
