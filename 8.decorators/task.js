eefe064133e72ad8ed9f4e63be827d09a246ec15
Dmitry Tarasov <vizalyyy@gmail.com>
2025-03-04 00:44:38 +0500

    Декораторы. Выполнено первое задание

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,30 @@
-//Задача № 1
+"use strict"
+
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+
+	function wrapper(...args) {
+		const hash = md5(args);
+		let objectInCache = cache.find((item) => item.hash === hash);
+		if (objectInCache) {
+			console.log("Из кеша" + objectInCache.value);
+			return "Из кеша: " + objectInCache.value;
+		}
+
+		let result = func(...args);
+		cache.push({
+			hash: hash,
+			value: result
+		});
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log("Вычисляем: " + result);
+		return "Вычисляем: " + result;
+	}
+	return wrapper;
 }
 
-//Задача № 2
 function debounceDecoratorNew(func, delay) {
   
 }
