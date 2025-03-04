07979035bfd774cd93676f2d135c4a79652f176c
Dmitry Tarasov <vizalyyy@gmail.com>
2025-03-04 23:07:24 +0500

    Ассинхронность. Доработки по рекомендациям

--- 7.async/task.js
+++ 7.async/task.js
@@ -27,10 +27,10 @@ class AlarmClock {
 	}
 
 	getCurrentFormattedTime() {
-		const date = new Date();
-		const hours = date.getHours().toString().padStart(2, "0");
-		const minutes = date.getMinutes().toString().padStart(2, "0");
-		return `${hours}:${minutes}`;
+		return new Date().toLocaleTimeString("ru-Ru", {
+			hour: "2-digit",
+			minute: "2-digit",
+		})
 	}
 
 	start() {
